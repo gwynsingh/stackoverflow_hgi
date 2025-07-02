47018a1cac589bb0c5986e8142f2aff1d7df9d45
parikshit-shaktawat <parikshit@edralabs.com>
2025-07-02 17:37:02 +0530

    Redirect, Env Fix

--- lib/stackoverflow_web/controllers/page_controller.ex
+++ lib/stackoverflow_web/controllers/page_controller.ex
@@ -23,9 +23,7 @@ defmodule StackoverflowWeb.PageController do
   end
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    redirect(conn, to: "/questions")
   end
 
   def questions(conn, params) do
